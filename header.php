ad9dc795ef2ec8ada638d43f92cd87415f7d862c
Onojeta Brown <onosbrown.saved@gmail.com>
2023-06-05 07:29:31 +0100

    update

--- header.php
+++ header.php
@@ -54,7 +54,7 @@
 </nav>
 <div class="accessNav">
 <a href="" class="menuToggler"><i class="twi-bars1"></i></a>
-<a href="https://llacademy.ng/parent-dashboard" class="qu_btn">Student Portal</a>
+<a href="https://llacademy.ng/parent-dashboard" target="_blank" class="qu_btn">Student Portal</a>
 </div>
 <div class="navleft">
 <div class="icon_box_04">
@@ -139,7 +139,7 @@
 <div class="accessNav">
  <a href="javascript:void(0);" class="menuToggler"><i class="twi-bars1"></i></a>
 
-<a href="https://llacademy.ng/parent-dashboard" class="qu_btn">Student portal</a>
+<a href="https://llacademy.ng/parent-dashboard" target="_blank" class="qu_btn">Student portal</a>
 </div>
 </div>
 </div>
